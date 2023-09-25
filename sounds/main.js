4e040c29e6764b9529b0aa484b22d64086d69dc3
RyanCabeloDePaje <134975360+RyanCabeloDePaje@users.noreply.github.com>
2023-09-25 20:05:33 -0300

    .

--- sounds/main.js
+++ sounds/main.js
@@ -4,52 +4,52 @@ function TocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
 
-document.querySelector('.tecla_pom') .onclick = TocaSomPom;
+document.querySelector('.tecla_pom').onclick = TocaSomPom;
 
 function TocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla_Clap') .onclick = TocaSomPom;
+document.querySelector('.tecla_Clap').onclick = TocaSomPom;
 
 function TocaSomTim(){
     document.querySelector('#som_tecla_tim').play();
 }
 
-document.querySelector('.tecla_Tim') .onclick = TocaSomPom;
+document.querySelector('.tecla_Tim').onclick = TocaSomPom;
 
 function TocaSomPuff(){
     document.querySelector('#som_tecla_puff').play();
 }
 
-document.querySelector('.tecla_Puff') .onclick = TocaSomPom;
+document.querySelector('.tecla_Puff').onclick = TocaSomPom;
 
 function TocaSomSplash(){
     document.querySelector('#som_tecla_splash').play();
 }
 
-document.querySelector('.tecla_Splash') .onclick = TocaSomPom;
+document.querySelector('.tecla_Splash').onclick = TocaSomPom;
 
 function TocaSomToim(){
     document.querySelector('#som_tecla_toim').play();
 }
 
-document.querySelector('.tecla_Toim') .onclick = TocaSomPom;
+document.querySelector('.tecla_Toim').onclick = TocaSomPom;
 
 function TocaSomPsh(){
     document.querySelector('#som_tecla_psh').play();
 }
 
-document.querySelector('.tecla_Psh') .onclick = TocaSomPom;
+document.querySelector('.tecla_Psh').onclick = TocaSomPom;
 
 function TocaSomTic(){
     document.querySelector('#som_tecla_tic').play();
 }
 
-document.querySelector('.tecla_Tic') .onclick = TocaSomPom;
+document.querySelector('.tecla_Tic').onclick = TocaSomPom;
 
 function TocaSomTom(){
     document.querySelector('#som_tecla_tom').play();
 }
 
-document.querySelector('.tecla_Tom') .onclick = TocaSomPom;
+document.querySelector('.tecla_Tom').onclick = TocaSomPom;
