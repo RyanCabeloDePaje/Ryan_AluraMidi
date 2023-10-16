7e22efc9ae31a9b9cddc06a813354634e2f7115e
RyanCabeloDePaje <134975360+RyanCabeloDePaje@users.noreply.github.com>
2023-10-16 19:35:24 -0300

    .

--- main.js
+++ main.js
@@ -1,142 +1,14 @@
- function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-const ListaDeTeclas = document.querySelectorAll('.tecla_pom');
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
 while(contador < ListaDeTeclas.length) {
 
-    ListaDeTeclas[contador].onclick = tocaSomPom;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-
-function tocaSomClap () {
-    document.querySelector('#som_tecla_clap').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_clap');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomClap;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomTim () {
-    document.querySelector('#som_tecla_tim').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_tim');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomTim;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}function tocaSomPuff () {
-    document.querySelector('#som_tecla_puff').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_puff');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomPuff;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomSplash () {
-    document.querySelector('#som_tecla_Splash').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_splash');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomSplash;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomToim () {
-    document.querySelector('#som_tecla_toim').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_toim');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomToim;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomPsh () {
-    document.querySelector('#som_tecla_psh').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_psh');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomPsh;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomTic () {
-    document.querySelector('#som_tecla_tic').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_tic');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomTic;
-
-    contador = contador + 1;
-
-    console.log(contador);
-}
-function tocaSomTom () {
-    document.querySelector('#som_tecla_tom').play();
-}
-
-const ListaDeTeclas = document.querySelectorAll('.tecla_tom');
-
-let contador = 0;
-
-while(contador < ListaDeTeclas.length) {
-
-    ListaDeTeclas[contador].onclick = tocaSomTom;
+    ListaDeTeclas[contador].onclick = tocaSom;
 
     contador = contador + 1;
 
